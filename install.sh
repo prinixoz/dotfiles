7333d6fccfeb703a3bbd2a8d71310eff460fe1a9
Prinix Roy <prinixoz@proton.me>
2024-06-24 17:05:09 +0530

    06/24/24|17:05:09

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# installing package manager
+sudo pacman -S git stow
+cd /tmp/
+git clone "https://aur.archlinux.org/paru-bin" 
+cd /tmp/paru-bin
+makepkg -si
+
+
+## Adding my dotfiles and stowing it
+cd ~/.local/
+git clone https://github.com/prinixoz/dotfiles
+cd ~/.local/dotfiles
+stow -vt ~ config/ scripts/
