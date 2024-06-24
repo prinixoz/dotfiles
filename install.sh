bed0d037150b16d25c43ca4f7982a21ce03cfab1
Prinix Roy <prinixoz@proton.me>
2024-06-24 23:29:15 +0530

    06/24/24|23:29:15

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # installing package manager
 sudo pacman -S git stow
 cd /tmp/
@@ -13,5 +14,20 @@ git clone https://github.com/prinixoz/dotfiles
 cd ~/.local/dotfiles
 stow -vt ~ config/ scripts/
 
+## Setting caotic aur
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+sudo echo "
+###################################################################################################
+
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+
+###################################################################################################
+" >> /etc/pacman.conf
+
 ## Install Package
-sudo pacman -S $(curl "https://raw.githubusercontent.com/prinixoz/dotfiles/main/install.txt")
+sudo pacman -S $(cat install.txt)
