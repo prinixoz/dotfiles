b2edaab6fb6a0dfef38189e5352a9b6f4aa8baf8
prinixoz <prinixoz@proton.me>
2024-06-26 20:15:15 +0530

    06/26/24|20:15:15

--- config/.config/i3/scripts/startup.sh
+++ config/.config/i3/scripts/startup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 gummy start &
 gummy -T 1 -y 06:00 -u 16:30 --temperature-min 2000 --temperature-max 5000 &
+swayidle before-sleep lock timeout 150 lock &
 dunst &
 nm-applet &
 battery-notify &
