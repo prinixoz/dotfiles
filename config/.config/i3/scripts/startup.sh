01d0e8ecf2fd12128e5861ca13d8ea21e512ca25
prinixoz <prinixoz@proton.me>
2025-07-11 18:03:49 +0530

    07/11/25|18:03:49

--- config/.config/i3/scripts/startup.sh
+++ config/.config/i3/scripts/startup.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 dbus-update-activation-environment --systemd DBUS_SESSION_BUS_ADDRESS DISPLAY XAUTHORITY &
-#gummy start &
-#gummy -T 1 -y 06:00 -u 16:30 --temperature-min 2000 --temperature-max 5000 &
 swayidle before-sleep lock &
 wlsunset -l  28.62190 -L 77.08784 &
 dunst &
 battery-notify &
 udiskie &
-yt-toggle &
+# yt-toggle &
