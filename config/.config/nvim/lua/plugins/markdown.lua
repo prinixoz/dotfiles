b202b846ec1f064b30e29a05793dfe80679f0b90
prinixoz <prinixoz@gmail.com>
2025-08-04 12:33:01 +0530

    08/04/25|12:33:01

--- config/.config/nvim/lua/plugins/markdown.lua
+++ config/.config/nvim/lua/plugins/markdown.lua
@@ -1,14 +1,39 @@
 return {
     {
-        "iamcco/markdown-preview.nvim",
-        ft = { "markdown" },
-        build = "cd app && npm install",
+        "epwalsh/obsidian.nvim",
+        version = "*", -- recommended, use latest release instead of latest commit
+        lazy = true,
+        ft = "markdown",
         config = function()
-            vim.g.mkdp_browser = "markdownBrowser" -- change to preferred browser here
-            vim.g.mkdp_auto_start = 0
-            vim.g.mkdp_auto_close = 1
-            vim.g.mkdp_open_to_the_world = 0
-            vim.g.mkdp_theme = "dark"
+            vim.opt_local.conceallevel = 2
+            require("obsidian").setup({
+                workspaces = {
+                    {
+                        name = "work",
+                        path = "/mnt/PS/Notes/",
+                    },
+                },
+                ui = {
+                    enable = false
+                },
+            })
         end,
+
+
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
     },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        config = function()
+        end,
+        ft = { "markdown" },
+    },
+
 }
--- config/.config/nvim/lua/plugins/treesitter.lua
+++ config/.config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,14 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    dependencies = { "OXY2DEV/markview.nvim" },
+    lazy = false,
     opts = {
         ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
         highlight = { enable = true },
     },
     config = function(_, opts)
         require("nvim-treesitter.configs").setup(opts)
+        vim.opt_local.conceallevel = 2
     end,
 }
